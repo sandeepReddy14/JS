242a3acd73dd05f6597639859b8e4f4b2115fc56
sandeepReddy14 <sandeepreddybalireddy@gmail.com>
2023-11-12 01:12:03 +0530

    Basic syntax of JS

---
+++ JS Fundamentals/basics.js
@@ -0,0 +1,26 @@
+/* 
+Whitespace:-
+Whitespace refers to the characters that provide space between other characters.
+Carriage return, space, tab, new line are whitespace in javascript. JavaScript 
+engine ignore whitespace, but whitespaces are used to format code and make it 
+more readable.
+
+Statement:-
+A statment is a code that instructs a javascript engine to do a task. It is terminated
+by a semicolon(;) although semicolon is optional it is a good practice to use it.
+
+Block:-
+A block is a sequence of simple statements delimited by a pair of curly braces {}.
+
+Identifiers:-
+Identifier is a name picked for a variable, parameters, functions etc; 
+
+
+*/
+// single line comment
+/* Block comment */
+
+let formatted = true
+if (formatted) {
+    console.log("Code is formatted.")
+}
